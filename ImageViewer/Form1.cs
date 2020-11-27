39fa82fa5c5937aabeca48cb71ba7609f98278cb
martafonde <marta4510@hotmail.com>
2020-11-27 21:36:17 +0100

    v1

---
+++ ImageViewer/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que quieres salir", "ImageVisor", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath;
+
+            using (OpenFileDialog op = new OpenFileDialog())
+            {
+                op.Title = "Select image";
+                op.InitialDirectory = Environment.GetEnvironmentVariable("USERPROFILE");
+                op.Filter = "jpg (*.jpg)|*.jpg|png (*.png)|*.png|All files (*.*)|*.*";
+
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = op.FileName;
+                    if (filePath.EndsWith(".jpg") || filePath.EndsWith(".png"))
+                    {
+                        if (abrirImagen(filePath))
+                        {
+                            Form2 f2 = new Form2(filePath);
+                            if (checkBox1.Checked)
+                            {
+                                f2.ShowDialog();     //modal
+                            }
+                            else
+                            {
+                                f2.Show();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Archivo no válido. Selecciona una imagen", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private bool abrirImagen(string path)
+        {
+            try
+            {
+                Image i = Image.FromFile(path);
+            }
+            catch (Exception ex) when (ex is ArgumentException || ex is OutOfMemoryException)
+            {
+                MessageBox.Show("Archivo no válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ImageViewer/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class Form2 : Form
+    {
+        public string Path { set; get; }
+        public Form2(string path)
+        {
+            InitializeComponent();
+            this.Path = path;
+            pictureBox1.Image = new Bitmap(Path);
+            this.ClientSize = new System.Drawing.Size(pictureBox1.Image.Size.Width, pictureBox1.Image.Size.Height);
+            FileInfo file = new FileInfo(Path);
+            this.Text = file.Name;
+        }
+
+        private void Form2_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.ClientSize.Width;
+            pictureBox1.Height = this.ClientSize.Height;
+
+        }
+    }
+}
